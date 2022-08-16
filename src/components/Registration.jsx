d96ccf1fa9b5df8c16f64182099e562d2408f3f2
Guilherme Varela <guilhermevareladossantos@gmail.com>
2022-08-16 05:58:25 -0300

    Put the Register Button in the center

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -5,12 +5,14 @@ import styles from './RegistrationSection.module.css'
 
 export function Registration() {
   return (
-    <section className={styles.mainSection}>
-      <a href=""><h1>CLICK HERE FOR PROGRAM COST AND TO FILL OUT THE REGISTRATION FORM!</h1></a>
-      <button>
+    <section className={styles.mainSection} id="registration">
+      <a target="blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfHNgHgoFXzi7mWOwsLmWC7mpbYZXkOUbK-8gbSSAMSh7GDpA/viewform">
+        <h1>CLICK HERE FOR PROGRAM COST AND TO FILL OUT THE REGISTRATION FORM!</h1>
+        <button>
         REGISTRATION 
         <Pencil size={30} weight='bold'/>
       </button>
+      </a>
     </section>
   )
 }

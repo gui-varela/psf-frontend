df017a58777bcc9dd6b3a36316236fbd37445708
Guilherme Varela <guilhermevareladossantos@gmail.com>
2022-09-20 02:11:37 -0300

    Add a slider in HomeSection image

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -1,9 +1,57 @@
-import { ArrowRight } from 'phosphor-react'
+import { ArrowRight, CaretLeft, CaretRight } from 'phosphor-react'
 
-import bannerImage from '../assets/pro-skill-futsal-kids-hugging.jpg'
+import bannerImage1 from '../assets/pro-skill-futsal-kids-hugging.jpg'
+import bannerImage2 from '../assets/pro-skill-futsal-all-kids.jpg'
+import bannerImage3 from '../assets/boys-training-soccer.jpg'
+import bannerImage4 from '../assets/boys-hugging.png'
 import styles from './HomeSection.module.css'
+import { useState } from 'react'
 
 export function HomeSection() {
+  const bannerImages = [
+    {
+      image: bannerImage1,
+      description: ""
+    },
+    {
+      image: bannerImage2,
+      description: ""
+    },
+    {
+      image: bannerImage3,
+      description: ""
+    },
+    {
+      image: bannerImage4,
+      description: ""
+    },
+  ]
+
+  const [counter, setCounter] = useState(0);
+  const [className, setClassName] = useState(styles.bannerImage);
+
+  function handleNextBannerImage() {
+    setClassName(styles.changingBannerImage)
+    setTimeout(function(){ 
+      setCounter(counter+1)
+      if (counter == bannerImages.length - 1)
+        setCounter(0)
+      console.log(counter)
+      setClassName(styles.bannerImage)
+    }, 1000);
+  }
+
+  function handlePreviousBannerImage() {
+    setClassName(styles.changingBannerImage)
+    setTimeout(function(){ 
+      setCounter(counter-1)
+    if (counter == 0)
+      setCounter(bannerImages.length - 1)
+    console.log(counter)
+    setClassName(styles.bannerImage)
+    }, 1000);
+  }
+
   return (
     <main className={styles.main}>
       <section className={styles.section}>
@@ -16,12 +64,25 @@ export function HomeSection() {
       </section> 
             
       <aside className={styles.aside}>
-        <div className={styles.imageContainer}>
-          <img 
-            className={styles.bannerImage} 
-            src={bannerImage} 
-            alt="kids hugging in front of the goalpost"
-          />
+        <div className={styles.slider}>
+          <CaretLeft onClick={handlePreviousBannerImage} size={40}/>
+          <div className={styles.imageContainer}>
+            <img 
+              className={className} 
+              src={bannerImages[counter].image}
+              alt={bannerImages[counter].description}
+            />
+          </div>
+          <CaretRight onClick={handleNextBannerImage} size={40}/>
+        </div>
+        <div className={styles.sliderPosition}>
+          {
+            bannerImages.map((image) => {
+              if (bannerImages.indexOf(image) == counter)
+                return <div key={bannerImages.indexOf(image)} className={styles.selectedBall}></div>
+              return <div key={bannerImages.indexOf(image)} className={styles.ball}></div>
+            })
+          }
         </div>
       </aside>
     </main>

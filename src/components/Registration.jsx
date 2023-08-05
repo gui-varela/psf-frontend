29df9a902357759f86d39af4042dbb6091437fd0
Guilherme Varela <guilhermevareladossantos@gmail.com>
2023-08-05 12:37:21 -0300

    Changes registration link

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -6,7 +6,7 @@ import styles from './RegistrationSection.module.css'
 export function Registration() {
   return (
     <section className={styles.mainSection} id="registration">
-      <a target="blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfHNgHgoFXzi7mWOwsLmWC7mpbYZXkOUbK-8gbSSAMSh7GDpA/viewform">
+      <a target="blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfT2BQ9f1yYIM_ISU_7xioGSdsfacGxcPCF8_BDOEX0uSIjHw/viewform?fbclid=IwAR1Y5-uxfl-C-fHtN0nNdLw4KJNpBN39xr0Gj219VzNutJCeZpUI7WFmGAo">
         <h1>CLICK HERE FOR PROGRAM COST AND TO FILL OUT THE REGISTRATION FORM!</h1>
         <button>
         REGISTRATION 

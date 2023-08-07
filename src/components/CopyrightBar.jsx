25c6ad3a2127808bc1965296f5d4a6d53a8787cd
gvarela <guilhermevareladossantos@gmail.com>
2023-08-07 14:56:32 -0300

    Turn Copyright bar's year dinamic

--- src/components/CopyrightBar.jsx
+++ src/components/CopyrightBar.jsx
@@ -1,9 +1,18 @@
+import { useState, useEffect } from 'react'
 import styles from './CopyrightBar.module.css'
 
 export function CopyrightBar() {
+  const [currentYear, setCurrentYear] = useState(2023)
+
+  useEffect(() => {
+    const today = new Date()
+    setCurrentYear(today.getUTCFullYear())
+    console.log(today)
+  }, [currentYear]);
+
   return (
     <footer className={styles.mainSection}>
-      &copy; Pro Skills Futsal 2022 &nbsp;<a href="http://">by Guilherme Varela</a>
+      &copy; Pro Skills Futsal {currentYear} &nbsp;<a href="https://github.com/gui-varela" target='_blank'>by Guilherme Varela</a>
     </footer>
   )
 }
